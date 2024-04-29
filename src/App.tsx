afc31a9a205d1b4504bf6089b48f21ab24c89665
blueeon <i@blueeon.me>
2024-04-29 17:05:42 +0800

    🌈 style: update theme - update demo content - update some style

--- src/App.tsx
+++ src/App.tsx
@@ -42,114 +42,29 @@ function App() {
   //     </article>
   //   )
   // }
+
+
   const markdown = `
-
-  # 微信公众号排版工具
-  >微信公众号排版工具。问题或建议，请公众号留言。**[程序员翻身](#jump_8)**
-
-  建议使用 **Chrome** 浏览器，体验最佳效果。
-  
-  使用微信公众号编辑器有一个十分头疼的问题：粘贴出来的代码，格式错乱，空间小还带行号，而且特别丑。Markdown.com.cn编辑器能够解决这个问题。
-  
-  Markdown是一种轻量级的「标记语言」。
-  
-  请阅读下方文本熟悉工具使用方法，本文可直接拷贝到微信中预览。
-  
-  
-  ## 1 Markdown.com.cn 简介
-  
-  - 支持自定义样式的 Markdown 编辑器
-  - 支持微信公众号、知乎和稀土掘金
-  - 点击右上方对应图标，一键复制到各平台
-  
-  ## 2 Markdown语法教程
-  
-  ### 2.1 标题
-  
-  不同数量的\`#\`可以完成不同的标题，如下：
-  
-  # 一级标题
-  
-  ## 二级标题
-  
-  ### 三级标题
-  
-  ### 2.2 字体
-  
-  粗体、斜体、粗体和斜体，删除线，需要在文字前后加不同的标记符号。如下：
-  
-  **这个是粗体**
-  
-  *这个是斜体*
-  
-  ***这个是粗体加斜体***
-  
-  ~这里想用删除线~
-  
-  注：如果想给字体换颜色、字体或者居中显示，需要使用内嵌HTML来实现。
-  
-  ### 2.3 无序列表
-  
-  无序列表的使用，在符号\`-\`后加空格使用。如下：
-  
-  - 无序列表 1
-  - 无序列表 2
-  - 无序列表 3
-  
-  如果要控制列表的层级，则需要在符号\`-\`前使用空格。如下：
-  
-  - 无序列表 1
-  - 无序列表 2
-    - 无序列表 2.1
-    - 无序列表 2.2
-  
-  **由于微信原因，最多支持到二级列表**。
-  
-  ### 2.4 有序列表
-  
-  有序列表的使用，在数字及符号\`.\`后加空格后输入内容，如下：
-  
-  1. 有序列表 1
-  2. 有序列表 2
-  3. 有序列表 3
-  
-  ### 2.5 引用
-  
-  引用的格式是在符号\`> \`后面书写文字。如下：
-  
-  > 读一本好书，就是在和高尚的人谈话。 ——歌德
-  
-  > 雇用制度对工人不利，但工人根本无力摆脱这个制度。 ——阮一峰
-  
-  ### 2.6 代码
-  ~~~js
-  console.log('It works!')
-  ~~~
-  ### 2.7 链接
-  
-  微信公众号仅支持公众号文章链接，即域名为\`https://mp.weixin.qq.com/\`的合法链接。使用方法如下所示：
-  
-  对于该论述，欢迎读者查阅之前发过的文章，[你是《未来世界的幸存者》么？](https://mp.weixin.qq.com/s/s5IhxV2ooX3JN_X416nidA)
-  <a id="jump_8"></a >
-  ### 2.8 图片
-  
-  插入图片，格式如下：
-  
-  ![这里写图片描述](https://www.nginx.cn/wp-content/uploads/2020/03/qrcode_for_gh_82cf87d482f0_258.jpg)
-  
-  支持 jpg、png、gif、svg 等图片格式，**其中 svg 文件仅可在微信公众平台中使用**，svg 文件示例如下：
-  
-  ![](https://markdown.com.cn/images/i-am-svg.svg)
-  
-  支持图片**拖拽和截图粘贴**到编辑器中。
-  
-  注：支持图片 ***拖拽和截图粘贴*** 到编辑器中，仅支持 https 的图片，图片粘贴到微信时会自动上传微信服务器。
+# AI早报 - 4月29日
+![image](https://imageio.forbes.com/specials-images/imageserve/64b5825a5b9b4d3225e9bd15/artificial-intelligence--ai/960x0.jpg?format=jpg&width=1440)
+1. 元象公司发布多模态大型模型XVERSE-V:支持任意宽高比的图像输入，在多个权威评测中表现优异，并已开源。
+2. 通义千问团队开源千亿参数模型Qwen1.5-110B:采用Transformer解码器架构，支持多语言，具有高效注意力机制。
+3. 生数科技与清华大学发布视频大模型Vidu:采用Diffusion与Transformer融合架构，一键生成高清视频，国际领先。4.Mutable Al推出Auto Wiki v2:自动将代码转化为维基百科风格的文章:解决代码文档化难题。
+5. 谷歌在美国新建数据中心:计划投资30亿美元在印第安纳州建设数据中心园区，扩建弗吉尼亚州设施，推出人工智能机会基金。6.中国信息通信研究院发布汽车大模型标准:旨在规范和推动汽车行业智能化发展。
+7. Kimi chat移动端应用更新:版本1.2.1全面重构用户界面，引入新的浅色模式，提供舒适直观的体验。
   `
 
   return (
     <main>
       <Md2Poster theme="SpringGradientWave" canCopy size='mobile'>
-        <Md2PosterHeader>{new Date().toISOString().slice(0, 10)}</Md2PosterHeader>
+        <Md2PosterHeader className='flex justify-between items-center px-4'>
+          <span>
+            @LingGame
+          </span>
+          <span>
+            {new Date().toISOString().slice(0, 10)}
+          </span>
+        </Md2PosterHeader>
         <Md2PosterContent>{markdown}</Md2PosterContent>
         <Md2PosterFooter>Powered by BeePoster.com</Md2PosterFooter>
       </Md2Poster>
