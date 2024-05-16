3943ad72b1633ea0036df49b1c54dd0d285d2252
zhoushan <672563682@qq.com>
2024-05-17 00:39:04 +0800

    docs: edit props

--- example/src/app/docs/page.tsx
+++ example/src/app/docs/page.tsx
@@ -1,5 +1,10 @@
+import dynamic from 'next/dynamic'
 import MdDocs from '@/markdown/docs.mdx'
 import Section from '@/components/Section'
+
+const EditProps = dynamic(() => import('./editProps'), {
+  ssr: false,
+ })
 
 export default function Docs() {
   return (
@@ -7,6 +12,7 @@ export default function Docs() {
       <article className="prose lg:prose-xl">
         <MdDocs />
       </article>
+      <EditProps />
     </Section>
   )
 }
